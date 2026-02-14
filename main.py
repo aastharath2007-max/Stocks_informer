d0fa21b402e41c76bc98d3a9ccf290c532a04311
Aastha Rath <aastharath2007@gmail.com>
2026-02-14 16:10:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,34 @@
 import requests
-from twilio.rest import Client
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+import datetime
+# user_params={'token':'penpineappleapplepen','username':'butterfingers','agreeTermsOfService':'yes','notMinor':'yes'}
+# pixela_endpoint="https://pixe.la/v1/users"
+# response=requests.post(pixela_endpoint,json=user_params)
+# print(response.status_code)
+# graph_config={'id':'graph1',
+#               'name': 'Coding Hours',
+#               'unit':'hours',
+#               'type':'float',
+#               'color':'ajisai'
+#                 }
+headers={'X-USER-TOKEN':'penpineappleapplepen'}
+graph_endpoint='https://pixe.la/v1/users/butterfingers/graphs'
+# response=requests.post(graph_endpoint,json=graph_config,headers=headers)
+# print(response.status_code)
 
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-API_KEY_forstocks='SY27Q41RARCYFY8D' #stock
-API_KEY_fornews= "cbd2239b2d7f4f1ea4d2d2230b77e8e7"    #news
-stock_params={'function':'TIME_SERIES_DAILY', 'apikey':API_KEY_forstocks,'symbol':STOCK}
-news_params={'apiKey':API_KEY_fornews,'q':'stocks','qInTitle':COMPANY_NAME}
+#Adding a Pixel
+# pixel_creation_endpoint='https://pixe.la/v1/users/butterfingers/graphs/graph1'
+# pixel_config={'date':'20260213',
+#               'quantity':'5'}
+# response=requests.post(pixel_creation_endpoint,json=pixel_config,headers=headers)
+# print(response.status_code)
 
-
-
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-
-
-stock_behaviour = requests.get(url=STOCK_ENDPOINT,params=stock_params)
-stock = stock_behaviour.json()
-
-st_yesterday = float(stock['Time Series (Daily)']['2026-01-30']['4. close'])
-st_day_before_yesterday= float(stock['Time Series (Daily)']['2026-01-29']['4. close'])
-print(stock)
-
-growth=st_yesterday-st_day_before_yesterday
-print(abs(growth))
-up_down=None
-
-
-percentage_growth=growth/st_yesterday*100
-if percentage_growth>0:
-    up_down='⬆️'
-else:
-    up_down='⬇️'
-
-#HINT 2: Work out the value of 5% of yesterday's closing stock price.
-cl_st_price=5/100*st_yesterday
-print(cl_st_price)
-
-if cl_st_price>5:
-    print('Get news')
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME.
-#HINT 1: Think about using the Python Slice Operator
-
-response=requests.get(url=NEWS_ENDPOINT,params=news_params)
-news=response.json()
-print(news)
-three_articles=news['articles'][:2]
-print(three_articles)
-
-#[new item for item in list]
-formatted_articles=[f'{STOCK}: {up_down}{percentage_growth}% \nHeadline:{article['title']} \nBrief:{article['description']}'for article in three_articles]
-print(formatted_articles)
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number.
-#HINT 1: Consider using a List Comprehension.
+#Updating a pixel
+update_endpoint='https://pixe.la/v1/users/butterfingers/graphs/graph1/20260214'
+# update_config={'quantity':'5'}
+# response=requests.put(update_endpoint,json=update_config,headers=headers)
+# print(response.status_code)
 
 
-
-
-client=Client(account_sid,auth_token)
-for article in formatted_articles:
-    message=client.messages.create(body=f'{article}',from_='+18126377684',to='+919434792435')
-    print(message.status)
-#Optional: Format the SMS message like this:
-"""
-TSLA: ⬆️2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: ⬇️5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+#Deleting a pixel
+response=requests.delete(update_endpoint,headers=headers)
+print(response.status_code)
